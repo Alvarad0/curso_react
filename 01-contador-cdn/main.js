e46b1863fe52c1f64efb07ba16785f9b90cef78c
Alvarado <alvarado.programador@gmail.com>
2021-10-25 12:39:44 -0500

    Contador usando CDN de React

---
+++ 01-contador-cdn/main.js
@@ -0,0 +1,16 @@
+const domContainer = document.getElementById("root")
+const Contador = () => {
+    const [numero, setNumero] = React.useState(0)
+    const aumentar = () => setNumero(numero+1)
+    const disminuir = () => setNumero(numero-1)
+    return(
+        <div>
+            <h1 className={numero<0 ? "menor" : "mayor"}>Contador: {numero}</h1>
+            <hr/>
+            <button onClick={aumentar}>Aumentar</button>
+            <button onClick={disminuir}>Disminuir</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<Contador/>, domContainer)
